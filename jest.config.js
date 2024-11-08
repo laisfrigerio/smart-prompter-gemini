d6e394c9048e20dee4c6cc43ddb7e2ec21d86e9b
Lais Frigerio <lais.frigerio@nubank.com.br>
2024-11-08 05:44:01 -0300

    adjusments of min  coverage config

--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,10 @@ module.exports = {
   testMatch: ['**/*.test.ts'],
   coverageThreshold: {
     global: {
-      branches: 95,
-      functions: 95,
-      lines: 95,
-      statements: 95
+      branches: 65,
+      functions: 90,
+      lines: 90,
+      statements: 90
     }
   }
 };
