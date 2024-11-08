fd99361b480e8d3721bd29ce801a5e6079ee6146
Lais Frigerio <lais.frigerio@nubank.com.br>
2024-11-08 19:08:44 -0300

    Configurando o coverage dos testes

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,12 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
 };
