857497fe48888dd28c6e617e0e84a773bcde05f8
Lais Frigerio <lais.frigerio@nubank.com.br>
2024-11-08 05:33:52 -0300

    Checking jest coverage

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,12 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 65,
+      functions: 65,
+      lines: 65,
+      statements: 65
+    }
+  }
 };
